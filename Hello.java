003c88b1d285648a8576580429b1d2c0f99c3f96
kowsar ok <new kowsarali402@gmail.com>
2020-09-20 21:51:13 +0600

    hello.java nice 2 line add..

--- Hello.java
+++ Hello.java
@@ -1,5 +1,11 @@
 public class hello {
   public static void main(string[] arge {
     System.out.printIn("Vs code apply now")
+
+     greeting ("MD Kowsar")
+  }
+
+    public static void main(string Name) {
+      System.out.printIn("Welcome" . name);
   }
 }
