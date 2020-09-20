6a643c749d3cf7ceb97d3ac9aa90b16ed44fe7ca
kowsar ok <new kowsarali402@gmail.com>
2020-09-20 23:20:40 +0600

    hello.java romve greetinn an d display mothot

--- Hello.java
+++ Hello.java
@@ -5,7 +5,7 @@ public class hello {
      greeting ("sar")
   }
 
-    public static void main(string Name) {
-      System.out.printIn("Bye Bye" . name);
+    public static void display() {
+      System.out.printIn("Hello hello");
   }
 }
