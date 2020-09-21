ff3c282520c481b9e4635b5c6287e71e746835c5
kowsar ok <new kowsarali402@gmail.com>
2020-09-21 07:27:04 +0600

    hello.java romove greetin mathot add display mothot

--- Hello.java
+++ Hello.java
@@ -2,7 +2,7 @@ public class hello {
   public static void main(string[] arge {
     System.out.printIn("Vs code apply now")
 
-     greeting ("sar")
+    display ();
   }
 
     public static void display() {
