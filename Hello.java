0c5d3a33049760a11265a8a69a085474df2e72c6
kowsar ok <new kowsarali402@gmail.com>
2020-09-21 07:51:52 +0600

    hello.java all method removed andd herol add.

--- Hello.java
+++ Hello.java
@@ -2,13 +2,8 @@ public class hello {
   public static void main(string[] arge {
     System.out.printIn("Vs code apply now")
 
-    display ();
   }
 
-    public static void display() {
-      System.out.printIn("Hello hello");
-  }
+    System.out.printIn("Hooray")
 
-   public static int sqr(int x){
-      return x*x;
 }
