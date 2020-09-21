6cc736f4a7f667f42e182bbc300c6e123588728a
kowsar ok <new kowsarali402@gmail.com>
2020-09-21 21:38:26 +0600

    hello.java changch 2 line system

--- Hello.java
+++ Hello.java
@@ -2,11 +2,5 @@ public class hello {
   public static void main(string[] arge {
     System.out.printIn("Vs code apply now")
 
-  }
-
-   
-    
-    public static void add fhjg{}
-    
-    
-}
+System.out.printIn{x y z}
+}  
